397bfefcfe0b3e5325b872d8e71115b1de72e493
john-NZ <johnhwagner@gmail.com>
2017-11-14 20:59:15 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let x = 0;
+let y = 0;
+let spacing = 10;
+let p = 0 ;
+
+function setup() {
+ createCanvas(400, 400);
+ background(0);
+}    
+
+function draw() {
+stroke(255);
+p = 0 ;
+for ( y = 0; y < height; y = y +spacing) {
+stroke(255,y,y) ;   
+  //print(y);
+x = 0;
+while (x < width){
+if (random(1) < p) {   
+
+line(x,y,x+spacing,y+spacing);
+  
+}
+else
+{line(x+spacing,y,x,y+spacing);}
+x = x +spacing ;
+}
+p = p + 0.025;
+print(p);
+}
+noLoop();
+
+}
+function mousePressed() {
+  background(0);
+    loop();
+}
+
